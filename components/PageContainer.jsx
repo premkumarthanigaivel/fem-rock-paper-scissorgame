6355cbb7643c18783b5987ff665e5f65b84d8262
Prem Kumar Thanigaivel <tprem990@gmail.com>
2022-06-07 22:41:35 +0530

    height fix for mobile

--- components/PageContainer.jsx
+++ components/PageContainer.jsx
@@ -1,9 +1,9 @@
 const Page = ({ children }) => {
-  return (
-    <main className="relative flex h-screen w-screen flex-col items-center bg-gradient-radial">
-      {children}
-    </main>
-  )
+	return (
+		<main className="relative flex h-screen min-h-screen w-screen flex-col items-center bg-gradient-radial">
+			{children}
+		</main>
+	)
 }
 
 export default Page
--- components/RulesSection.jsx
+++ components/RulesSection.jsx
@@ -1,16 +1,16 @@
 const RulesSection = ({ setModalVisible }) => {
-  return (
-    <section className="absolute bottom-0 flex h-24 w-full items-center justify-center md:h-32 md:justify-end">
-      <div
-        className="mt-8 scale-100 rounded-md border-2 border-white px-8 py-2 tracking-wider text-white hover:scale-105 md:mt-0 md:mr-8"
-        onClick={() => {
-          setModalVisible(true)
-        }}
-      >
-        RULES
-      </div>
-    </section>
-  )
+	return (
+		<section className="mt-auto mb-10 flex w-full items-center justify-center md:absolute md:bottom-0 md:mb-0 md:h-32 md:justify-end">
+			<div
+				className="scale-100 rounded-md border-2 border-white px-8 py-2 tracking-wider text-white hover:scale-105 md:mt-8  md:mr-8"
+				onClick={() => {
+					setModalVisible(true)
+				}}
+			>
+				RULES
+			</div>
+		</section>
+	)
 }
 
 export default RulesSection
