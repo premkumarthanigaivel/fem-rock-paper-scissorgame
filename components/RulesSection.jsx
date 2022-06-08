891abb6e87410c52913309ff98f59ac823dd578c
Prem Kumar Thanigaivel <tprem990@gmail.com>
2022-06-08 22:19:52 +0530

    height fix

--- components/RulesSection.jsx
+++ components/RulesSection.jsx
@@ -1,6 +1,6 @@
 const RulesSection = ({ setModalVisible }) => {
 	return (
-		<section className="static mt-[7.5rem] mb-10 flex w-full items-center justify-center md:absolute md:bottom-0 md:mt-0 md:mb-0 md:h-32 md:justify-end">
+		<section className="static mt-[5.5rem] mb-10 flex w-full items-center justify-center md:absolute md:bottom-0 md:mt-0 md:mb-0 md:h-32 md:justify-end">
 			<div
 				className="scale-100 rounded-md border-2 border-white px-8 py-2 tracking-wider text-white hover:scale-105 md:mt-8  md:mr-8"
 				onClick={() => {
