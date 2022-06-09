512b8efdd2d1d35aba78bbdaae43a7ea4d000d80
Prem Kumar Thanigaivel <tprem990@gmail.com>
2022-06-09 23:46:12 +0530

    height fix for mobile

--- components/PageContainer.jsx
+++ components/PageContainer.jsx
@@ -1,6 +1,6 @@
 const Page = ({ children }) => {
 	return (
-		<main className="relative flex h-auto w-screen flex-col items-center bg-gradient-radial md:h-screen">
+		<main className="relative flex h-screen w-screen flex-col items-center bg-gradient-radial md:h-screen">
 			{children}
 		</main>
 	)
--- components/RulesSection.jsx
+++ components/RulesSection.jsx
@@ -1,8 +1,8 @@
 const RulesSection = ({ setModalVisible }) => {
 	return (
-		<section className="static mt-[5.5rem] mb-10 flex w-full items-center justify-center md:absolute md:bottom-0 md:mt-0 md:mb-0 md:h-32 md:justify-end">
+		<section className="absolute bottom-4 flex h-24 w-full items-center justify-center md:bottom-0 md:h-32 md:justify-end">
 			<div
-				className="scale-100 rounded-md border-2 border-white px-8 py-2 tracking-wider text-white hover:scale-105 md:mt-8  md:mr-8"
+				className="mt-8 scale-100 rounded-md border-2 border-white px-8 py-2 tracking-wider text-white hover:scale-105 md:mt-0 md:mr-8"
 				onClick={() => {
 					setModalVisible(true)
 				}}
