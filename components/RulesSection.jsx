7198f2694d501a4b6b48739bff2cd5d8a36242ff
Prem Kumar Thanigaivel <tprem990@gmail.com>
2022-06-09 23:52:58 +0530

    rules section bottom pos increase

--- components/RulesSection.jsx
+++ components/RulesSection.jsx
@@ -1,6 +1,6 @@
 const RulesSection = ({ setModalVisible }) => {
 	return (
-		<section className="absolute bottom-4 flex h-24 w-full items-center justify-center md:bottom-0 md:h-32 md:justify-end">
+		<section className="absolute bottom-16 flex h-24 w-full items-center justify-center md:bottom-0 md:h-32 md:justify-end">
 			<div
 				className="mt-8 scale-100 rounded-md border-2 border-white px-8 py-2 tracking-wider text-white hover:scale-105 md:mt-0 md:mr-8"
 				onClick={() => {
